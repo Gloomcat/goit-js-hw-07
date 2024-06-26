411e1bcccaf6e0285a5ac3351dac67ff7d35da15
Oleksii Aleksieiev <reneguarde@gmail.com>
2024-06-26 14:07:19 +0300

    Complete fifth task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+button.addEventListener('click', (_) => {
+    const newColor = getRandomHexColor();
+
+    const colorText = document.querySelector('.color');
+    colorText.textContent = newColor;
+
+    const body = document.querySelector('body');
+    body.style.backgroundColor = newColor;
+});
