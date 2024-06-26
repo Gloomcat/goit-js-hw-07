8a420d66df6db84902e56612bd04b0550a19ab4b
Oleksii Aleksieiev <reneguarde@gmail.com>
2024-06-26 12:53:01 +0300

    Complete third task

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+const inputField = document.querySelector('#name-input');
+const outputField = document.querySelector('#name-output');
+
+inputField.addEventListener('input', (event) => {
+    const content = event.currentTarget.value.trim();
+    outputField.textContent = content ? content : 'Anonymous';
+});
